2bd5cbb9c406868506917d799a6b84a9b7886328
Mohamed Santawi <mohamedsant3@gmail.com>
2025-09-22 23:32:12 +0300

    Fix Vercel build - add .NET SDK installation and build script

--- SafeOpsBlazor/SafeOpsWeb/build.sh
+++ SafeOpsBlazor/SafeOpsWeb/build.sh
@@ -6,6 +6,9 @@ curl -sSL https://dot.net/v1/dotnet-install.sh | bash /dev/stdin --channel 8.0
 # Add .NET to PATH
 export PATH="$HOME/.dotnet:$PATH"
 
+# Verify .NET installation
+dotnet --version
+
 # Restore dependencies
 dotnet restore
 
