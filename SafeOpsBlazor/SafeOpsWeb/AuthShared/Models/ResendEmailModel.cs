e6097ecc2dda764e2b3194b602fd3731afc1d230
Mohamed Santawi <mohamedsant3@gmail.com>
2025-09-22 23:06:50 +0300

    Copy AuthShared to SafeOpsWeb directory and update Dockerfile for Railway deployment

---
+++ SafeOpsBlazor/SafeOpsWeb/AuthShared/Models/ResendEmailModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AuthShared.Models;
+
+public class ResendEmailModel
+{
+    [Required]
+    [EmailAddress(ErrorMessageResourceName = "ValidationMessages", 
+        ErrorMessageResourceType = typeof(Resources.ValidationMessages),
+        ErrorMessage = "EmailError")]
+    public string Email { get; set; }
+}
---
+++ SafeOpsBlazor/SafeOpsWeb/AuthShared/Models/UserInformations.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthShared.Models
+{
+    public class UserInformations
+    {
+        public string Id { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        [MinLength(3)]
+        [MaxLength(20)]
+        public string UserName { get; set; }
+        public string Role { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string MiddleName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        //[RegularExpression("^\\d+$")]
+        public string PhoneNumber { get; set; }
+        [Required]
+        //[RegularExpression("^\\d+$")]
+        public string PhoneCode { get; set; }
+        public MajorSharedModel Major { get; set; }
+        public SectionSharedModel Section { get; set; }
+        public bool IsUserLocked { get; set; }
+
+
+
+    }
+}
